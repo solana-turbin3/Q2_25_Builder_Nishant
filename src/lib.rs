d650fa3d6612383997967d71295e52bedd141729
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-03-27 17:14:40 +0530

    base58 to wallet and wallet to base58 implement

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+use bs58;
 use solana_sdk::{
     pubkey::Pubkey,
     signature::{Keypair, Signer},
 };
+use std::io::{self, BufRead};
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -26,4 +28,43 @@ mod tests {
 
     #[test]
     fn transfer_sol() {}
+
+    #[test]
+    fn base58_to_wallet() {
+        println!("Input your private key as base58:");
+
+        let stdin = io::stdin();
+
+        let base58 = stdin.lock().lines().next().unwrap().unwrap();
+
+        println!("Your wallet file is:");
+
+        let wallet = bs58::decode(base58).into_vec().unwrap();
+
+        println!("{:?}", wallet);
+    }
+
+    #[test]
+    fn wallet_to_base58() {
+        println!("Input your private key as a wallet file byte array:");
+
+        let stdin = io::stdin();
+        let wallet = stdin
+            .lock()
+            .lines()
+            .next()
+            .unwrap()
+            .unwrap()
+            .trim_start_matches("[")
+            .trim_end_matches("]")
+            .split(",")
+            .map(|s| s.trim().parse::<u8>().unwrap())
+            .collect::<Vec<u8>>();
+
+        println!("You private key :");
+
+        let base58 = bs58::encode(wallet).into_string();
+
+        println!("{:?}", base58)
+    }
 }
