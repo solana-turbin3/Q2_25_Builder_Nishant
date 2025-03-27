448887a6d164c765bbfe0af25e27bab0e399da86
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-03-27 18:15:41 +0530

    Airdrop to wallet

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 use bs58;
+use solana_client::rpc_client::RpcClient;
 use solana_sdk::{
     pubkey::Pubkey,
-    signature::{Keypair, Signer},
+    signature::{Keypair, Signer, read_keypair_file},
 };
 use std::io::{self, BufRead};
+
+const RPC_URL: &str = "https://api.devnet.solana.com";
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -24,7 +27,27 @@ mod tests {
     }
 
     #[test]
-    fn airdrop() {}
+    fn airdrop() {
+        let keypair = read_keypair_file("wallet_key.json").expect("Couldn't find wallet file");
+
+        //connected to the devnet rpc client
+        let client = RpcClient::new(RPC_URL);
+
+        match client.request_airdrop(&keypair.pubkey(), 2_000_000_000) {
+            Ok(s) => {
+                println!("Success! Check out TX here:");
+
+                println!(
+                    "https://explorer.solana.com/tx/{}?cluster=devnet",
+                    s.to_string()
+                );
+            }
+
+            Err(e) => {
+                println!("Something went wrong : {}", e.to_string())
+            }
+        }
+    }
 
     #[test]
     fn transfer_sol() {}
