d957183be11ea6b39d784e9aad5bdbcc435b35b3
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-03-27 15:13:15 +0530

    add necessary test function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// pub fn add(left: u64, right: u64) -> u64 {
+//     left + right
+// }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
+
+    // #[test]
+    // fn it_works() {
+    //     let result = add(2, 2);
+    //     assert_eq!(result, 4);
+    // }
+
+    use solana_sdk;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn keygen() {}
+
+    #[test]
+    fn airdrop() {}
+
+    #[test]
+    fn transfer_sol() {}
 }
